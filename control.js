f5957d384d7ee916800ff4162bbc4e8cb7d002f6
Malukito5 <cleide960@gmail.com>
2024-10-23 16:35:33 +0100

    Update control.js

--- control.js
+++ control.js
@@ -1,6 +1,6 @@
 var user={
 	num : ['768',"980","333","891","888","501"],
-	senha: ["estrala","azul","festa","Brilho","vida","pacote"],
+	senha: ["estrela","azul","festa","Brilho","vida","pacote"],
 	time: [new Date("2024/10/28"),new Date("2024/10/25"),new Date("2024/10/27"),new Date("2024/10/24"),new Date("2024/11/21"), new Date("2024/10/25") ]
 };
 var casaAposta, h1;
